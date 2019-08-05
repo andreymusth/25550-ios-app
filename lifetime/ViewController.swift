a3806ffac61b5aad96415ffe4fcc630ee67e6a10
Andrey Belous <ABelous2@alfabank.ru>
2019-08-05 21:51:28 +0300

    initial commit

---
+++ lifetime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lifetime
+//
+//  Created by Andrey on 05/08/2019.
+//  Copyright © 2019 Latitude Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
