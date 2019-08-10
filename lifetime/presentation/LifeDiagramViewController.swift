25f96693f53aed7d3902bc2155f7463b4d612b0d
Andrey Belous <ABelous2@alfabank.ru>
2019-08-10 14:49:03 +0300

    added another screen

---
+++ lifetime/presentation/LifeDiagramViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LifeDiagramViewController.swift
+//  lifetime
+//
+//  Created by Andrey on 10/08/2019.
+//  Copyright © 2019 Latitude Apps. All rights reserved.
+//
+
+import UIKit
+
+class LifeDiagramViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        applyGradientBackground(rootView: self.view)
+    }
+
+}
