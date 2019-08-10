abdbc49ac72c061217d3929bed38429d7fac695f
Andrey Belous <ABelous2@alfabank.ru>
2019-08-10 21:40:58 +0300

    added translation animation for lifetime pin

--- lifetime/presentation/LifeDiagramViewController.swift
+++ lifetime/presentation/LifeDiagramViewController.swift
@@ -10,13 +10,29 @@ import UIKit
 
 class LifeDiagramViewController: UIViewController {
 
+    @IBOutlet weak var pinImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         applyGradientBackground(rootView: self.view)
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        animate()
+    }
+    
+    private func animate() {
+        let originalTransform = self.pinImageView.transform
+        let translatedTransform = originalTransform.translatedBy(x: 0.0, y: -250.0)
+        UIView.animate(withDuration: 0.7, animations: {
+            self.pinImageView.transform = translatedTransform
+        })
+    }
 
     @IBAction func settingsButtonClick(_ sender: Any) {
-       
+       dismiss(animated: false, completion: nil)
     }
 }
